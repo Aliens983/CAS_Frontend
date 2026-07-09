171d90cf3b327e1ffd3cbf5b4e14cc9487b3a392
Aliens983 <255169814@qq.com>
2026-07-09 10:21:34 +0800

    feat:Prepare to upload by docker.

--- src/common/utils/request.ts
+++ src/common/utils/request.ts
@@ -3,7 +3,7 @@ import { ElMessage } from 'element-plus'
 import { useUserStore } from '@/common/stores/user'
 
 const request: AxiosInstance = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL || '/api',
   timeout: 15000,
   headers: {
     'Content-Type': 'application/json',
